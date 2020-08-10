6d944697cc4a0608eb4f8b9e78bdf10e10aee273
ymunt <yana.muntianu@gmail.com>
2020-08-10 17:34:28 +0300

    Created TC - Valid data in input field

--- src/test/java/com/herokuapp/tests/TestHerokuAppInputs.java
+++ src/test/java/com/herokuapp/tests/TestHerokuAppInputs.java
@@ -1,6 +1,26 @@
 package com.herokuapp.tests;
 
+import com.herokuapp.pages.HerokuappInputPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestHerokuAppInputs extends TestSetup {
 
+    final static Logger LOG = LoggerFactory.getLogger(TestHerokuAppInputs.class);
 
+    @Test(testName = "Test input field on /inputs page")
+    public void inputValidDataIntoInputFieldTest() {
+
+        LOG.info("Start: Test input field on /inputs page ");
+
+        HerokuappInputPage inputPage = new HerokuappInputPage(getDriver());
+        inputPage.navigateToInputsPage()
+                .setInputText("1234");
+
+        Assert.assertEquals(inputPage.getTextFromInput(), "1234", "Text is different:");
+
+        LOG.info("Finished: Test input field on /inputs page ");
+    }
 }
